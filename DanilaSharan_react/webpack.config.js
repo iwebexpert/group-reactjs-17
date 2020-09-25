2e4f261007f4d067114a1d0cd22a6f084b24e9e1
dannilsan <dannilsan@gmail.com>
2020-09-25 20:34:35 +0700

    Lesson-2

--- DanilaSharan_react/webpack.config.js
+++ DanilaSharan_react/webpack.config.js
@@ -1,32 +1,39 @@
 const path = require('path')
-const HTMLWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports ={
-  entry: path.join(__dirname, 'src', 'index.js'),
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+      alias: {
+        components: path.join(__dirname, 'src', 'components'),
+      },
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
+    module: {
+        rules: [
+          {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+                plugins: ['@babel/plugin-proposal-class-properties'],
+              }
+            }
           }
-        }
-      }
-    ]
-  },
+        ]
+      },
 
-  plugins: [
-    new HTMLWebpackPlugin({
-      template: path.join(__dirname, 'src', 'index.html'),
-      filename: "index.html",
-    })
-  ],
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html'),
+            filename: 'index.html',
+        }),
+    ],
 }
