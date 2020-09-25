b7735f7ad05bb1dd356b0013edceaf4b0eaf02ba
loonarinc <loonarinc@gmail.com>
2020-09-25 09:59:50 +0300

    Lesson-02

---
+++ loonar/src/components/Message.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export class Message extends Component {
+
+    render () {
+        return (
+            <div>
+                <p>{this.props.text} <b>({this.props.author})</b></p>
+            </div>
+        );
+    }
+};
---
+++ loonar/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+// import {App} from 'components/App';
+// import {App2} from 'components/App2';
+import {Messenger} from 'components/Messenger';
+
+ReactDom.render(
+    <>
+        <Messenger />
+    </>, 
+    document.getElementById('root')
+);
