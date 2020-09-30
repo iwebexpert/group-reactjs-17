bf6d3c35cb9ab99b64645e8718ea431c8057423a
Rastrapon <58354560+Rastraponovich@users.noreply.github.com>
2020-09-30 11:09:45 +0300

    lesson 4

--- KuroeDovVitalii/src/Components/Header/Header.jsx
+++ KuroeDovVitalii/src/Components/Header/Header.jsx
@@ -1,19 +1,23 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import { AppBar, Toolbar, IconButton, Typography, Button, ButtonGroup } from '@material-ui/core' 
-
-class Header extends Component {
-    render(){
-        const title = this.props.title
+import Profile from '../Profile/Profile'
+import NewChat from '../NewChat/NewChat'
+export default class Header extends Component {
+    render() {
+        const Title = this.props.chatName !== null ? this.props.chatName : this.props.title
         return(
             <AppBar position="static">
                 <Toolbar variant="dense">
+                    <Profile user={this.props.user} handleNameChange={this.props.handleNameChange}/>
                     <Typography variant="h6" >
-                        {this.props.title}
+                        { Title }
                     </Typography>
+                    <ButtonGroup className="button-group" variant="contained">
+                        <Button variant="outlined">Чаты</Button>
+                        <NewChat user={this.props.user} users={this.props.users} handleNewChat={this.props.handleNewChat}/>
+                    </ButtonGroup>
                 </Toolbar>
             </AppBar>
         )
     }
 }
-
-export default Header
