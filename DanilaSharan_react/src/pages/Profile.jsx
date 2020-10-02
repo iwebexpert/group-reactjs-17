98b32358507b71d19426b36cc3398ef2c11a08ce
dannilsan <dannilsan@gmail.com>
2020-10-02 20:49:16 +0700

    Lesson-4

---
+++ DanilaSharan_react/src/pages/Profile.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './page.css';
+
+export function Profile() {
+  return (
+    <div className="text">
+      <h1>Profile</h1>
+      <p>Hello, User, this is your profile!</p>
+    </div>
+  )
+}
