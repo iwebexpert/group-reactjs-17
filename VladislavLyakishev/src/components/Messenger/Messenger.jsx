035e5c948b8011efe6ca927e462eb93de8124296
Владислав Лякишев <vlad169@yandex.ru>
2020-10-02 13:43:57 +0300

    Create add message in select chat

--- VladislavLyakishev/src/components/Messenger/Messenger.jsx
+++ VladislavLyakishev/src/components/Messenger/Messenger.jsx
@@ -21,39 +21,62 @@ export class Messenger extends Component {
   };
 
   componentDidUpdate() {
-    const botAnswer = {
-      message: this.state.messageData[this.state.messageData.length - 1].author + ' Спасибо за ваше сообщение, но в чате никого нет, кроме меня...',
-      author: 'BOT',
-      id: nanoid()
-    };
-    const {author, id} = this.state.messageData[this.state.messageData.length - 1]
-    if (author !== 'BOT') {
-      const timerBotAnswer = setTimeout( () => {
-        if (id === this.state.messageData[this.state.messageData.length - 1].id) {
-          this.setState({
-            messageData: this.state.messageData.concat(botAnswer)
-          })
-        }
-      }, 2000)
+    // const botAnswer = {
+    //   message: this.state.messageData[this.state.messageData.length - 1].author + ' Спасибо за ваше сообщение, но в чате никого нет, кроме меня...',
+    //   author: 'BOT',
+    //   id: nanoid()
+    // };
+    // const {author, id} = this.state.messageData[this.state.messageData.length - 1]
+    // if (author !== 'BOT') {
+    //   const timerBotAnswer = setTimeout( () => {
+    //     if (id === this.state.messageData[this.state.messageData.length - 1].id) {
+    //       this.setState({
+    //         messageData: this.state.messageData.concat(botAnswer)
+    //       })
+    //     }
+    //   }, 2000)
+    // }
+  }
+
+  get messages() {
+    const {chats} = this.state
+    const {id} = this.props.match.params
+
+    let messages = []
+
+    if (id && chats[id]) {
+      messages = chats[id].messages
     }
+    return messages
   }
 
   handleMessageSend = (message) => {
+    const {chats} = this.state
+    const {id} = this.props.match.params
+
+    const chat = chats[id]
     message.id = nanoid();
+    chat.messages = this.messages.concat(message)
+
+
     this.setState({
-      messageData: this.state.messageData.concat(message)
+      chats: {
+        ...chats,
+        [chat]: chat
+      }
     })
+    console.log(this.state.chats)
   };
 
 
   render() {
-    const {messageData} = this.state;
+    const messageData = this.messages;
     return (
       <div className="messenger">
         <div className='messenger-list'>
-          <MessageList messageData={messageData}/>
+          {messageData.length > 0 ? <MessageList messageData={messageData}/> : <div className="messenger-empty">Выберите чат в списке слева</div>}
         </div>
-        <MessengerField onSend={this.handleMessageSend}/>
+        {messageData.length > 0 ? <MessengerField onSend={this.handleMessageSend}/> : <div></div>}
       </div>
     )
   }
