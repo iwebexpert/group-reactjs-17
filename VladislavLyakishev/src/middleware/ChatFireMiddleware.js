bf20adf6b45e8f9efed44e4b39492a76d7fea959
vlworks <vlad169@yandex.ru>
2020-10-09 15:28:18 +0300

    Create ChatUnFire correct bug

--- VladislavLyakishev/src/middleware/ChatFireMiddleware.js
+++ VladislavLyakishev/src/middleware/ChatFireMiddleware.js
@@ -4,15 +4,10 @@ export const ChatFireMiddleware = store => next => action => {
   const CHANGE_LOCATION = '@@router/LOCATION_CHANGE'
   if (action.type === CHANGE_LOCATION) {
     const path = action.payload.location.pathname
-    // Проверка что мы в чатах, но тут будет баг если число чатов перевалит за 10
-    if (path.slice(0, -1) === '/chats/') {
-      store.dispatch(chatsToggleUnFire(path.slice(-1)))
+    // Проверка что мы в чатах
+    if (path.split('/')[1] === 'chats') {
+      store.dispatch(chatsToggleUnFire(path.split('/')[2]))
     }
-
-
-    // if (currentLocation !== `/chats/${chatId}`) {
-    //     store.dispatch(chatsToggleFire(chatId))
-    //  }
   }
   return next(action)
 }
