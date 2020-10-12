1efc3dfefad7f7abec61393c26a2bf6411479b13
Rastrapon <58354560+Rastraponovich@users.noreply.github.com>
2020-10-12 14:16:16 +0300

    bot answer fix

--- KuroeDovVitalii/src/middlewares/botMiddleWare.js
+++ KuroeDovVitalii/src/middlewares/botMiddleWare.js
@@ -3,11 +3,14 @@ import { nanoid } from 'nanoid'
 
 import { citates } from '../helpers/citates'
 
+let timeOutId = 0
+
 export const botMiddleWare = store => next => action => {
     if (action.type === CHATS_MESSAGE_SEND) {
         const { chatId, id, name, numSelectedChat } = action.payload
         if (name === 'me') {
-            setTimeout(() => {
+            clearTimeout(timeOutId)
+            timeOutId = setTimeout(() => {
                 store.dispatch(chatsMessageSendAction({
                     name: 'БОТ', 
                     text: citates[Math.floor(Math.random() * 10) + 1].text, 
