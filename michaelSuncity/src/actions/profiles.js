c1a14ed6a0f8cc6740223dafd694aaebf01fe9b4
MichaelSuncity <mixail.pismarov@mail.ru>
2020-10-13 14:32:30 +0300

    homework 7 профиль на fetch

--- michaelSuncity/src/actions/profiles.js
+++ michaelSuncity/src/actions/profiles.js
@@ -9,7 +9,7 @@ export const PROFILES_LOAD = 'PROFILES_LOAD';
 export const PROFILES_LOAD_REQUEST = 'PROFILES_LOAD_REQUEST';
 export const PROFILES_LOAD_SUCCESS = 'PROFILES_LOAD_SUCCESS';
 export const PROFILES_LOAD_FAILURE = 'PROFILES_LOAD_FAILURE';
-
+/*
 export const profilesLoadAction = () => createAction({
     endpoint: 'http://localhost:3000/profiles',
     method: 'GET',
@@ -20,4 +20,30 @@ export const profilesLoadAction = () => createAction({
         PROFILES_LOAD_SUCCESS,
         PROFILES_LOAD_FAILURE,
     ],
+});*/
+
+export const profilesLoadRequestAction = () =>({
+    type: PROFILES_LOAD_REQUEST,
 });
+
+export const profilesLoadSuccessAction = (data) =>({
+    type: PROFILES_LOAD_SUCCESS,
+    payload: data,
+});
+
+export const profilesLoadFailureAction = (error) =>({
+    type: PROFILES_LOAD_FAILURE,
+    payload: error,
+});
+
+export const profilesLoadAction = () => {
+    return async (dispatch) => {
+        try {
+            dispatch(profilesLoadRequestAction());
+            const result = await fetch('http://localhost:3000/profiles');
+            dispatch(profilesLoadSuccessAction(await result.json()));
+        } catch(error){
+            dispatch(profilesLoadFailureAction(error));
+        }
+    }
+};
