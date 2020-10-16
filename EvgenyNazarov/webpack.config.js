2db9071b9f150e8059fddec459461d0346bcd2cf
Nazarov1987 <45045147+Nazarov1987@users.noreply.github.com>
2020-10-16 14:18:00 +0300

    Практическое занятие 8
    Функционал по состоянию на 7 урок. За предстоящие выходные добавлю функционал.

--- EvgenyNazarov/webpack.config.js
+++ EvgenyNazarov/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
     entry: path.join(__dirname, 'src', 'index.js'),
@@ -52,6 +53,13 @@ module.exports = {
         }),
         new MiniCssExtractPlugin({
           filename: 'main.css',
+        }),
+        new CopyPlugin({
+          patterns: [
+            { from: path.join(__dirname, 'src', 'assets', 'images'), to: path.join(__dirname, 'dist', 'images') },
+            { from: path.join(__dirname, 'src', 'sw.js'), to: path.join(__dirname, 'dist') },
+            { from: path.join(__dirname, 'src', 'manifest.json'), to: path.join(__dirname, 'dist') },
+          ],
         }),
     ],
 
