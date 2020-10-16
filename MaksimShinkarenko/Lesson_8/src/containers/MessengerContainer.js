d9c00efe42e55ce90141118682ced6be252bf1fa
ShinkarenkoMaksim <makc.pst@gmail.com>
2020-10-16 19:39:18 +0300

    Init

---
+++ MaksimShinkarenko/Lesson_8/src/containers/MessengerContainer.js
@@ -0,0 +1,85 @@
+import React from "react";
+import {connect} from 'react-redux';
+import {nanoid} from "nanoid";
+import {push} from 'connected-react-router';
+
+import {Messenger} from 'components/Messenger';
+import {chatsLoadAction, chatsMessageAction, chatsAddAction} from 'actions/chats';
+
+class MessengerContainerClass extends React.Component {
+    componentDidMount() {
+        const {chatsLoadAction, chats} = this.props;
+        if (!chats.length)
+            chatsLoadAction();
+    }
+
+    handleChatAdd = (chat) => {
+        chat.id = this.props.chats.length;
+        chat.fire = false;
+        const {chatsAddAction, redirect} = this.props;
+        chatsAddAction(chat);
+        redirect(chat.id);
+    }
+
+    handleMessageSend = (message) => {
+        message.id = nanoid();
+        message.chatId = this.props.chatId;
+        const {chatsMessageAction} = this.props;
+        chatsMessageAction(message);
+    };
+
+    handleChatsReload = () => {
+        this.props.chatsLoadAction();
+    };
+
+    render() {
+        const {chats, messages, isLoading, isError} = this.props;
+        return (
+            <Messenger chats={chats}
+                       messages={messages}
+                       isLoading={isLoading}
+                       isError={isError}
+                       handleMessageSend={this.handleMessageSend}
+                       handleChatAdd={this.handleChatAdd}
+                       handleChatsReload={this.handleChatsReload}
+            />
+        );
+    }
+}
+
+function mapStateToProps(state, ownProps) {
+    const chats = state.chats.entries;
+    const {match} = ownProps;
+
+    let messages = null;
+
+    if (match && chats[match.params.id]) {
+        messages = chats[match.params.id].messages;
+    }
+
+    const chatsArray = [];
+    for (let key in chats) {
+        if (chats.hasOwnProperty(key)) {
+            chatsArray.push({title: chats[key].title, id: chats[key].id, fire: chats[key].fire});
+        }
+    }
+
+    return {
+        chats: chatsArray,
+        messages,
+        chatId: match ? match.params.id : null,
+        isLoading: state.chats.loading,
+        isError: state.chats.error,
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        chatsLoadAction: () => dispatch(chatsLoadAction()),
+        chatsMessageAction: (message) => dispatch(chatsMessageAction(message)),
+        chatsAddAction: (chat) => dispatch(chatsAddAction(chat)),
+        redirect: (chatId) => dispatch(push(`/chats/${chatId}`)),
+    };
+}
+
+export const MessengerContainer = connect(mapStateToProps, mapDispatchToProps)(MessengerContainerClass);
---
+++ MaksimShinkarenko/Lesson_8/src/reducers/chats.js
@@ -0,0 +1,95 @@
+import update from 'react-addons-update';
+
+import {
+    CHATS_MESSAGE_SEND,
+    CHATS_LOAD_FAILURE,
+    CHATS_LOAD_SUCCESS,
+    CHATS_LOAD_REQUEST,
+    CHATS_ADD,
+    CHATS_UNFIRE,
+    CHATS_FIRE
+} from 'actions/chats';
+
+const initialState = {
+    loading: false,
+    error: false,
+    entries: [],
+}
+
+export const chatsReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case CHATS_LOAD_REQUEST:
+            return {
+                ...state,
+                loading: true,
+                error: false,
+            };
+
+        case CHATS_LOAD_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                entries: action.payload,
+            };
+
+        case CHATS_LOAD_FAILURE:
+            return {
+                ...state,
+                loading: false,
+                error: true,
+            };
+
+        case CHATS_MESSAGE_SEND:
+            return update(state, {
+                entries: {
+                    [action.payload.chatId]: {
+                        messages: {
+                            $push: [{
+                                id: action.payload.id,
+                                text: action.payload.text,
+                                author: action.payload.author
+                            }],
+                        },
+                    },
+                },
+            });
+
+        case CHATS_ADD:
+            return update(state, {
+                entries: {
+                    $push: [{
+                        id: action.payload.id,
+                        title: action.payload.title,
+                        messages: [
+                            {
+                                id: 0,
+                                author: 'WebDev',
+                                text: `Приветствуем в новом чате - ${action.payload.title}`
+                            },
+                        ],
+                    }]
+                }
+            });
+
+        case CHATS_FIRE:
+            return update(state, {
+                entries: {
+                    [action.payload]: {
+                        $merge: {fire: true}
+                    }
+                }
+            });
+
+        case CHATS_UNFIRE:
+            return update(state, {
+                entries: {
+                    [action.payload]: {
+                        $merge: {fire: false}
+                    }
+                }
+            });
+
+        default:
+            return state;
+    }
+}
